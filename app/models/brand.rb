bfe4d86fd27f0ee572d948c2a907677505e7cbcf
ryonakajima-25 <nov.5x5@gmail.com>
2020-01-10 08:56:07 +0900

    edit

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,5 @@
 class Brand < ApplicationRecord
   has_many :products
-  has_many :categorys, through :brand_category
+  has_many :categorys, through :brand_categorys
   has_many :brand_categorys
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   has_many :products
-  has_many :brands, through :brand_category
+  has_many :brands, through :brand_categorys
   has_many :brand_categorys
 end
