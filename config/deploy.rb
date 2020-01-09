10b7bc926973f11613fa46b093982748d51afa1f
taka1127 <taka07251127@gmail.com>
2020-01-10 02:31:42 +0900

    ベーシック認証設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,13 @@ set :keep_releases, 5
 
 set :linked_files, %w{ config/secrets.yml }
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
