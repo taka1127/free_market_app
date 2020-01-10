c4f89348071eb1fa9676674a40cb13363091a81f
taka1127 <taka07251127@gmail.com>
2020-01-10 16:58:57 +0900

    Revert "ベーシック認証"
    This reverts commit 11cb0e767e377c4538abc93b465452c4f57cbd80.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :basic_auth
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == '66a' && password == '6611'
-    end
-  end
 end
