85ad8b6efc03e1cb0bcbf4ec0ebdd2cecef4c33c
taka1127 <taka07251127@gmail.com>
2020-01-10 16:58:52 +0900

    Revert "ベーシック認証設定"
    This reverts commit 10b7bc926973f11613fa46b093982748d51afa1f.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  before_action :basic_auth
 
   private
 
-  def production?
-    Rails.env.production?
-  end
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == '66a' && password == '6611'
     end
   end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,1 @@
 server '18.177.99.221', user: 'ec2-user', roles: %w{app db web}
-
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
