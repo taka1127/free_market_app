4cc906116e0c45ad686b594354d3a52fc2588dc7
yamassx <58455442+yamassx@users.noreply.github.com>
2020-01-12 22:16:14 +0900

    非ログイン時のリダイレクト先を変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,7 @@ class ProductsController < ApplicationController
 private
 
   def access_registration
-    # リダイレクト先をトップページに仮置き
-    redirect_to "#" unless user_signed_in?　
+    redirect_to new_user_session_path, unless user_signed_in?　
   end
 
 end
