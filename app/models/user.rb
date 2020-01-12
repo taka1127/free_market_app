8b97010f7b6ea1fef5f82a9efb535cba0ea86947
yamassx <58455442+yamassx@users.noreply.github.com>
2020-01-12 20:08:38 +0900

    omniaouth関連エラーの応急処置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable
+        #  :omniauthable
   has_many :goods
   has_many :products
   has_many :comments
