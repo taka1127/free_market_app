7469a7f44502658ef85cac19ed5c8000510361c5
yamassx <58455442+yamassx@users.noreply.github.com>
2020-01-12 22:02:47 +0900

    非ログインユーザーのアクセス制限
    ・トップページと商品詳細ページ以外にはログインしなければ入れないように実装。現状二つのコントローラーのみなので、他のモデルにアクションが必要な場合は設定変更の必要がありそうです。
    ・ビューができていないので、現状動作確認ができない状況。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :access_registration, except: [:index, :show]
+
   def index
-    
   end
+
+  def show
+  end
+
+private
+
+  def access_registration
+    # リダイレクト先をトップページに仮置き
+    redirect_to "#" unless user_signed_in?　
+  end
+
 end
