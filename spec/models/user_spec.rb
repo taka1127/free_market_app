73bbd558fda2be41e5c844ef961f6cb1ed178ab9
yamassx <58455442+yamassx@users.noreply.github.com>
2020-01-14 16:20:46 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,42 +4,42 @@ describe User do
     it "is invalid without a nickname" do
       user = build(:user, nickname: nil)
       user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      expect(user.errors[:nickname]).to include("を入力してください")
     end
 
     it "is invalid without a mail_address" do
       user = build(:user, email: nil)
       user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      expect(user.errors[:email]).to include("を入力してください")
     end
 
     it "is invalid without a name" do
       user = build(:user, name: nil)
       user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
+      expect(user.errors[:name]).to include("を入力してください")
     end
     
     it "is invalid without a name_kana" do
       user = build(:user, name_kana: nil)
       user.valid?
-      expect(user.errors[:name_kana]).to include("can't be blank")
+      expect(user.errors[:name_kana]).to include("を入力してください")
     end
 
     it "is invalid without a tel" do
       user = build(:user, tel: nil)
       user.valid?
-      expect(user.errors[:tel]).to include("can't be blank")
+      expect(user.errors[:tel]).to include("を入力してください")
     end
 
     it "is invalid without a birthday" do
       user = build(:user, birthday: nil)
       user.valid?
-      expect(user.errors[:birthday]).to include("can't be blank")
+      expect(user.errors[:birthday]).to include("を入力してください")
     end
     it "is invalid without a password" do
       user = build(:user, password: nil)
       user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      expect(user.errors[:password]).to include("を入力してください")
     end
   end
 end
