62afda48139865cce33de96affb9f5fa432b8bff
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2020-01-15 17:12:26 +0900

    インスタンス変数完成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,10 @@ class ProductsController < ApplicationController
   end
   
   def show
+    @product = Product.find("id: params")
+    @image = Image.find("id: params")
+    @category = Category.find("id: params")
+    @brand = Brand.select("brand")
   end
   
   def edit
@@ -17,5 +21,20 @@ private
       redirect_to new_user_session_path, unless user_signed_in?　
       end
     end
-
 end
+
+# インスタンス変数前（今後、記述は消す予定）
+# @productname = Product.select("name")
+# @productimage = Image.select("image")
+# @username = 
+# @categoyl = Category.select("category_L")
+# @categorym = Category.select("category_M")
+# @categorys = Category.select("category_S")
+# @brand = Brand.select("brand")
+# @status = Product.select("status")
+# @deliverycharge = Product.select("delivery_charge")
+# @shippingmethod = Product.select("shipping_method")
+# @prefecture = Product.select("prefecture")
+# @shipmentdate = Product.select("date_of_shipment")
+# @productprice = Product.select("price")
+# @productcontent = Product.select("content")
