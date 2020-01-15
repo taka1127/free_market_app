94c0220a2b71bdc67eceacdf0dab65c71e02b9aa
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2020-01-15 17:27:32 +0900

    ビュー記述変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,19 +22,3 @@ private
       end
     end
 end
-
-# インスタンス変数前（今後、記述は消す予定）
-# @productname = Product.select("name")
-# @productimage = Image.select("image")
-# @username = 
-# @categoyl = Category.select("category_L")
-# @categorym = Category.select("category_M")
-# @categorys = Category.select("category_S")
-# @brand = Brand.select("brand")
-# @status = Product.select("status")
-# @deliverycharge = Product.select("delivery_charge")
-# @shippingmethod = Product.select("shipping_method")
-# @prefecture = Product.select("prefecture")
-# @shipmentdate = Product.select("date_of_shipment")
-# @productprice = Product.select("price")
-# @productcontent = Product.select("content")
