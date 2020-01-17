0f3c24d4c6932cbe90e360dabce8d26e5087ef78
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2020-01-17 13:57:31 +0900

    テスト記述

---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe ProductsController do
+  describe 'GET #edit' do
+    # it "renders the :edit template" do
+    #   expect(response).to render_template :edit
+    # end
+  end
+
+end
