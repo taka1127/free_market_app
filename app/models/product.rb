43cfe7cc3b6620d05de63f1f65c69ea6e72683db
ryonakajima-25 <nov.5x5@gmail.com>
2020-01-17 13:19:15 +0900

    開発環境動作確認→微修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :name, :content, :size, :status, :s_charge, :s_method, :s_prefecture, 
-            :s_date, :price, :category_L ,:category_M, :category_S
+            :s_date, :price, :category_L ,:category_M, :category_S,
             presence: true
   # belongs_to :brand
   # belongs_to :category
