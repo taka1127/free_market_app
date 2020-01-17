5de9c5e40b11daccdce2c06494910e606ad8d389
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2020-01-17 11:20:08 +0900

    一部修正

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
-# describe ProductsController do
-#   describe 'GET #show' do
-#     it "renders the :show template" do
-#       get :show, params: {  id: 1 }
-#       expect(response).to render_template :show
-#     end
+describe ProductsController do
+  describe 'GET #new' do
+    end
 
 end
