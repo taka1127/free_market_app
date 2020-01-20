e4a44263be9219ccaec54fa2263a91066beb417c
taka1127 <taka07251127@gmail.com>
2020-01-20 21:15:37 +0900

    新規登録画面のlink_to設定

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,21 @@
+class SignupController < ApplicationController
+
+  def user_top
+  end
+
+  def session1
+  end
+
+  def session2
+  end
+
+  def session3
+  end
+
+  def session4
+  end
+
+  def session5
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   root "products#index"
   resources :products,  only: [:index, :new, :create, :edit, :update, :show]
   resources :users, only: [:show]
+  resources :signup, only: [:index] do
+    collection do
+      get 'user_top'
+      get 'session1'
+      get 'session2'
+      get 'session3'
+      get 'session4' 
+      get 'session5' 
+    end
+  end
 end
