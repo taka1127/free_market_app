0f6eae761c9da84399689c3631818fa8232461ff
taka1127 <taka07251127@gmail.com>
2020-01-22 22:00:24 +0900

    edit

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,10 +1,12 @@
 class SignupController < ApplicationController
-
+  # before_action :authenticate_user!
   def user_top
   end
 
   def session1
     @user = User.new 
+    # @user = User.find(params[:id])
+    # @address = @user.addresses.build  ## 親モデル.子モデル.buildという形式
     # @user.addresses.new
   end
 
@@ -22,6 +24,7 @@ class SignupController < ApplicationController
     session[:tel] = user_params[:tel]
     @user = User.new 
     @address = Address.new
+    # @address = @user.address.build
   end
 
   def session4
@@ -34,10 +37,15 @@ class SignupController < ApplicationController
     session[:city] = address_params[:city]
     session[:house_number] = address_params[:house_number]
     session[:building] = address_params[:building]
+    session[:user_id] = address_params[:user_id]
+    # binding.pry
     @address = Address.new
+    # @address = @user.address.build
   end
 
   def create
+    # @address = @user.address.build
+
     @user = User.new(
       # session1のデータ
       nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
@@ -50,23 +58,37 @@ class SignupController < ApplicationController
       name: session[:name], 
       name_kana: session[:name_kana], 
     )
+    # @address = @user.address.build
+    # @address = @user.addresses.build(
+    #   postal_code: session[:postal_code], 
+    #   prefecture: session[:prefecture], 
+    #   city: session[:city], 
+    #   house_number: session[:house_number], 
+    #   building: session[:building], 
+      # tel: session[:tel]addressカラムに作っていないのでコメントアウト
+      # session4のデータ(クレジットカード情報)未実装
+    # )
+
     @address = Address.new(
       postal_code: session[:postal_code], 
       prefecture: session[:prefecture], 
       city: session[:city], 
       house_number: session[:house_number], 
-      building: session[:building], 
-      # user_id: current_user.id
+      building: session[:building],
       # tel: session[:tel]addressカラムに作っていないのでコメントアウト
       # session4のデータ(クレジットカード情報)未実装
     )
+    # @address = @user.address.build
+
     if @user.save && @address.save
       # ログインするための情報を保管
       session[:id] = @user.id
+      # @address = @user.address.build
+      # @address = @user.addresses.build
+      # session[:user_id] = @user.id 
       redirect_to session5_signup_index_path
     else
       redirect_to root_path
-      # render '/signup/registration'
     end
   end
 
@@ -81,8 +103,7 @@ class SignupController < ApplicationController
   end
 
   def address_params
-    params.require(:user).require(:addresses).permit(:postal_code,:prefecture,:city,:house_number,:building )
-    # params.require(:user).require(:addresses).permit(:postal_code,:prefecture,:city,:house_number,:building ).merge(user_id: current_user.id)
+    params.require(:user).require(:addresses).permit(:user_id,:postal_code,:prefecture,:city,:house_number,:building )
   end
 
 end
