290d9cbfa8036e575e46048ad2be3f6c62a71ab9
ryonakajima-25 <nov.5x5@gmail.com>
2020-01-23 21:55:14 +0900

    DB修正

--- db/migrate/20200110092835_create_products.rb
+++ db/migrate/20200110092835_create_products.rb
@@ -6,14 +6,14 @@ class CreateProducts < ActiveRecord::Migration[5.0]
       t.text    :content,          null: false
       # t.integer :category_id,      null: false, foreign_key: true
       t.string  :size
-      t.string :brand_name,         foreign_key: true
+      t.string :brand_name
       t.string :status,           null: false
       t.string :s_charge,         null: false
       t.string :s_method,         null: false
       t.string  :s_prefecture,     null: false
       t.string :s_date,           null: false
       t.integer :price,            null: false
-      t.string  :category,
+      t.string  :category
       t.references :user,          foreign_key: true
       t.timestamps
     end
