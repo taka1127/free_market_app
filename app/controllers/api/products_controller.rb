f916e2d2000d2fb63a1e8d2438f8cef4d62a9bac
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2020-01-24 17:17:59 +0900

    API実装２

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,4 +1,15 @@
 class Api::ProductsController < ApplicationController
   def index
   end
+ 
+  def pay
+     Payjp.api_key = 'sk_test_96f14e0e07de7024eedd09ec'
+     Payjp::Charge.create(
+       #amountは値段を記載
+       amount: 400000, 
+       card: params['payjp-token'],
+       currency: 'jpy'
+     )
+   end
+
 end
