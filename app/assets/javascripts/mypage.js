7f3689e19cf3c9e65a23dfc8adad4bfefe61e910
taka1127 <taka07251127@gmail.com>
2020-01-27 21:56:25 +0900

    edit

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,18 +1,25 @@
-$(".info").on("click",function(){
-  var $th = $(this).index();
-  $("#panel_1").removeClass("active");
-  $("#panel_2").removeClass("active");
-  $(this).addClass("active");
-  $(".nomessage").eq($th).addClass("active");
+
+
+$(document).on('turbolinks:load', function() {
+	$('.mypage-tab').on('click', function(){
+		$('.is-active').removeClass('is-active');
+		$(this).toggleClass('is-active');
+		$('.is-show').removeClass('is-show');
+    const index = $(this).index();
+    $('.mypage-panel').eq(index).toggleClass('is-show');
+    console.log($('.mypage-panel'))
+    return false;    
+	});
 });
 
-
-// $(document).on('turbolinks:load', ()=> {  
-//   $(".info").on("click",function(){
-//     var $th = $(this).index();
-//     $("#panel_1").removeClass("active");
-//     $("#panel_2").removeClass("active");
-//     $(this).addClass("active");
-//     $(".nomessage").eq($th).addClass("active");
-//   });
+// $(document).on('turbolinks:load', function() {
+// 	$('.mypage-tab2').on('click', function(){
+// 		$('.is-active2').removeClass('is-active2');
+// 		$(this).toggleClass('is-active2');
+// 		$('.is-show2').removeClass('is-show2');
+// 		const index = $(this).index();
+//     $('.mypage-panel2').eq(index).toggleClass('is-show2');
+//     return false;    
+// 	});
 // });
+
