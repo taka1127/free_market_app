9924113d7d414f9e130fefdb6a7ebc1c76d24f41
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2020-01-29 15:02:00 +0900

    JS修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,22 +1,19 @@
 class CardsController < ApplicationController
   require 'payjp'
 
-  # 後ほど登録したクレジットの表示画面を作成します。
   def index
   end
 
-  # クレジットカード情報入力画面
   def new
     @card = Card.new
   end
 
-  # 登録画面で入力した情報をDBに保存
   def create
     Payjp.api_key = ENV['payjp_key_secret']
     if params['payjp-token'].blank?
       render 'mypage/index'
     else
-      customer = Payjp::Customer.create( # ここで先ほど生成したトークンを顧客情報と紐付け、PAY.JP管理サイトに送信
+      customer = Payjp::Customer.create(
         card: params['payjp-token'],
       )
       @card = Card.new(user_id: current_user.id, customer_id: customer[:id])
